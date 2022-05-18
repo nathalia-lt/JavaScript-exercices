5bd455a03b94e264656785d5683ccf231231fa51
nathalia <nathaliatroina@gmail.com>
2022-05-18 11:11:42 -0400

    explicacao dos loops ou array interation

---
+++ 0518/arrays-1.js
@@ -0,0 +1,84 @@
+// Array Iteration:
+
+let movies = ['The Maltese Falcon', 'The Big Sleep', 'Casablanca', 'Key Largo']
+// to get a specfic movie, you can call to its index -> movies[i]
+
+console.log('THE SECOND MOVIE: ',movies[0])
+
+// option 1: 
+console.log('MOVIES 1 : ')
+//  for (start, stop, (what we do after all of the logic inside the loop executes)  )
+for (let i=0; i < movies.length; i++){ //os ii e para percorrer por toda array
+    console.log(movies[i])
+    if (movies[i]==='Casablanca') console.log(i)
+}
+
+// option 2:
+console.log('MOVIES 2: ')
+movies.forEach(movie=>console.log(movie))//for each vai em um por um na array (like map but different)
+
+// option 3:
+console.log('MOVIES 3: ')
+movies.map(movie=>console.log(movie))
+
+
+// option 4:
+console.log('MOVIES 4: ')
+
+for (let movie of movies){ //o jeito mais facil de ir em um por um na array
+    console.log(movie)
+}
+
+
+
+
+// INCLUDES
+// .includes(element) -> returns true or false if the specified element is in the array
+ let title = 'The Godfather'
+let includes1 = movies.includes(title)
+
+console.log('INCLUDES 1: ', includes1)
+
+let includes2 = movies.includes('The Big Sleep')
+
+console.log('INCLUDES 2: ', includes2)
+
+
+
+// INDEXOF
+// .indexOf(element) -> returns the index of the specified element
+// if the element is not in the array, returns -1
+
+let indexOf1 = movies.indexOf('The Big Sleep') //com indexOf, se verifica se o que estamos procurando
+//esta dentro da array
+
+console.log('INDEX OF 1: ', indexOf1)
+
+let indexOf2 = movies.indexOf('The Godfather')
+
+console.log('INDEX OF 2: ', indexOf2)
+
+
+//CREATING A RANGE OF NUMBERS 
+
+//Normally you would do this:
+let num = 10
+
+let nums = []
+for (let i=0; i <10; i++){
+    nums.push(i)
+}
+
+console.log('FOR LOOP: ',nums)
+
+//ALTERNATIVE:
+
+// Only use this in specific circumstances when you need an array of numbers from 0 to num
+
+let nums2 = [...Array(num).keys()]
+
+for (let num of nums2){
+    console.log(movies[num])
+}
+
+console.log('ALTERNATIVE: ', nums2)
